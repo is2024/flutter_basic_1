9309864d597241dbcc7e880fe891ac50365db922
is2024 <is.mohidin@gmail.com>
2024-05-15 12:22:06 +0800

    git init flutter basic

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Hello Flutter Kelas B'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemBuilder: (context, index){
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(backgroundColor: Color.fromARGB(255, 248, 41, 5), child: Text('$index'),
+              ), 
+              title: Text('Ismail'),
+              // subtitle: Text('$index'),
+              subtitle: Text('Sedang Belajar Flutter'),
+            ),
+          );
+        },
+         itemCount: 20,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
