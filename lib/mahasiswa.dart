285a555fad83252e10f2897e4fb84a4061a8913d
is2024 <is.mohidin@gmail.com>
2024-05-28 13:26:34 +0800

    git init fluter basic

---
+++ lib/mahasiswa.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Mahasiswa {
+  
+  int nim;
+  String nama;
+  String address
+  ;
+  Mahasiswa({
+    required this.nim,
+    required this.nama,
+    required this.address,
+  });
+  
+}
+
